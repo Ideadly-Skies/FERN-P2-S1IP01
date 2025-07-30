26f4b5ca81ebb4d0fc325c33ceef04cb49915e86
Ideadly-Skies <Obie.kal22@gmail.com>
2025-07-31 00:21:51 +1000

    refactor: BuyersHomePage layout

--- src/pages/buyers/BuyersHomePage.jsx
+++ src/pages/buyers/BuyersHomePage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 function BuyersHomePage() {
   return (
-    <div>BuyersHomePage</div>
-  )
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '80vh', 
+      background: '#f5f7fa' 
+    }}>
+      <h1 style={{ color: '#2d3748', marginBottom: '0.5em' }}>Welcome to Buyers Home Page</h1>
+      <p style={{ color: '#4a5568', fontSize: '1.2em' }}>
+        Explore the best deals and find your perfect purchase!
+      </p>
+    </div>
+  );
 }
 
-export default BuyersHomePage
+export default BuyersHomePage;
